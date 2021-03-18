ba0e34defab01d42e73ee66da2633b8c62199f19
nchloride <clankz39@gmail.com>
2021-03-18 22:42:51 +0800

    fixed: authentication bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,23 +5,11 @@ import cookie from "cookie"
 import { useEffect } from 'react';
 const jwt = require("jsonwebtoken");
 
-const Redirect = ({to}) =>{
-  const router = useRouter();
 
-  useEffect(() => {
-    router.push(`/${to}`);
-    console.log(to);
-  }, [])
-  return <h1>Loading...</h1>
-}
 
-export default function Home({page,isAuthenticated}){
-  useEffect(() => {
-    console.log(page,isAuthenticated);
-    
-  }, [])
-  if(isAuthenticated) return <Redirect to={page}/>
-  
+export default function Home(){
+
+
   return (
     <Login/>
   )
@@ -34,9 +22,9 @@ export const getServerSideProps = async ({req,res})=>{
     const employee = await jwt.verify(parsedCookie.token,process.env.TOKEN_KEY);
       if(employee){
         return{
-            props:{
-              page: employee.role,
-              isAuthenticated:true
+            redirect:{
+              destination:`/${employee.role}`,
+              permanent:false
             }
          }
       } 
