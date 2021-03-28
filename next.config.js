1bc4b175cf719784fa2e8d58fea6032507dfe787
nchloride <clankz39@gmail.com>
2021-03-28 23:41:32 +0800

    added: / redirect to dashboard

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
-const { redirect } = require("next/dist/next-server/server/api-utils");
 
 module.exports = {
     env:{
         MONGO_URI:process.env.MONGO_URI,
         TOKEN_KEY:process.env.TOKEN_KEY
     },
-
+    async redirects(){
+        return[
+            {
+                source:"/admin",
+                destination:"/admin/dashboard",
+                permanent:false
+            }
+        ]
+    }
 }
