979725b2500d49c9937dd0afda66b9bd4021a6f3
nchloride <clankz39@gmail.com>
2021-03-28 23:41:00 +0800

    added: user credentials

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -1,11 +1,20 @@
+import { useEffect } from "react";
 import NavigationBar from "../components/Admin/NavigationBar";
 
-export const Layout = ({children})=>{
-
+export const Layout = ({children,user})=>{
+    useEffect(()=>{
+        console.log("user: ",user);
+        
+    },[])
     return (
         <div className="layout">
-            <NavigationBar/>
-            {children}
+            <div className="layout_header">
+                <h1>{user.name}</h1>
+            </div>
+            <div className="layout_main">
+                <NavigationBar/>
+                {children}
+            </div>
         </div>
     )  
 }
