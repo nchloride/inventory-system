47ec68186c2c5c1fba5830606f0767b4bac8f0f8
nchloride <clankz39@gmail.com>
2021-06-26 14:34:12 +0800

    Rename files/ Auth to Schema

--- src/utils/validations/storesAuth.ts
+++
@@ -1,9 +0,0 @@
-import Joi from "joi";
-
-const storeSchema = Joi.object({
-    branch:Joi.string().required(),
-    location:Joi.string().required(),
-    status:Joi.string().required()
-})
-
-export default storeSchema;
