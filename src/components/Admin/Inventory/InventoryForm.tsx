bb533fb0255d6c519f0d26d5d6a23c21a869c444
nchloride <clankz39@gmail.com>
2021-07-08 16:24:52 +0800

    Add product name list on form

--- src/components/Admin/Inventory/InventoryForm.tsx
+++ src/components/Admin/Inventory/InventoryForm.tsx
@@ -4,10 +4,20 @@ import {useState,useContext} from "react";
 import {useForm} from "react-hook-form"
 
 import {InventoryContext} from "../../../pages/admin/inventory";
+import { useRef } from "react";
 
-export default function InventoryForm({branches,modalOpen,setModalOpen}){
+interface ISelectedProduct{
+    _id:string,
+    name:string,
+    stocks:number,
+    price:number
+}
+Modal.setAppElement("body");
+export default function InventoryForm({branches,modalOpen,setModalOpen,products}){
     const {handleSubmit,register,reset} = useForm();
     const inventory = useContext(InventoryContext);
+    const [selectedProduct,setSelectedProduct] = useState<ISelectedProduct>();
+    const productOption = useRef();
     const onFormSubmit = async(data:any) =>{
         inventory.setStocks(data)
             .then(data=>{
@@ -16,8 +26,16 @@ export default function InventoryForm({branches,modalOpen,setModalOpen}){
             }
         );
     }
-    Modal.setAppElement("body");
-    const closeModal = () => setModalOpen(!modalOpen)
+    const closeModal = () => setModalOpen(!modalOpen);
+
+    const handleSelectOnChange = (e) =>{
+        if(e.target.value !== "0"){
+            const productData = products.filter(product=> product.name === e.target.value);
+            console.log(productData[0]);
+            
+            setSelectedProduct(productData[0]);
+        }
+    }
     return (
         <Modal isOpen={modalOpen} className="modal_form inventory_modal">
             <div className="modal_title">
@@ -37,12 +55,31 @@ export default function InventoryForm({branches,modalOpen,setModalOpen}){
                         </option>
                     ))}
                 </select>
-                <label>Stocks:</label>
-                <input type="number" name="stocks" placeholder="Stocks" ref={register({required:true})} ></input>
                 <label>Product name:</label>
-                <input type="text" name="name" placeholder="Name" ref={register({required:true})} ></input>
-                <label>Price:</label>
-                <input type="number" name="price" placeholder="Price" ref={register({required:true})} ></input>
+                <select name="name" ref={register({required:true})} onChange={handleSelectOnChange}>
+                    <option value="0">---Select Product---</option>
+                    {products.map((product,id)=>(
+                        <option ref={productOption} key={id} value={product.name}>{product.name}</option>
+                    ))}
+                </select>
+                {
+                    selectedProduct && (
+                        <>
+                            <div className="selected__product">
+                                <div className="selected__product__card">
+                                    <label>Price:</label>
+                                    {selectedProduct.price+'php'}
+                                </div>
+                                <div className="selected__product__card">
+                                    <label>Stock:</label>
+                                    {selectedProduct.stocks+"pcs"}
+                                </div>
+                            </div>
+                            <label>Stocks:</label>
+                            <input type="number" name="stocks" placeholder="Stocks" max={selectedProduct?.stocks || 0} ref={register({required:true})} ></input>
+                        </>
+                    )
+                }
                 <input type="submit" value="Assign product"></input>
             </form>
         </Modal>
